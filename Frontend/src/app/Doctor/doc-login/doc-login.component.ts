9bba9f413ca88ff8ecabfbeaebdf6155823303b3
Prarthana <artikumari1974@gmail.com>
2022-01-31 16:21:49 +0530

    DocUI

---
+++ Frontend/src/app/Doctor/doc-login/doc-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-login',
+  templateUrl: './doc-login.component.html',
+  styleUrls: ['./doc-login.component.css']
+})
+export class DocLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Frontend/src/app/Doctor/doc-signup/doc-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-signup',
+  templateUrl: './doc-signup.component.html',
+  styleUrls: ['./doc-signup.component.css']
+})
+export class DocSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
